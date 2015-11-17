69417a6b1561331ce3a7aa36cca46140d58f1d0d
vijayanand17 <vijayanand.p@live.com>
2015-11-18 04:06:17 +0530

    Logging
    Logging some debug messages

--- createsqldbuser.ps1
+++ createsqldbuser.ps1
@@ -10,7 +10,7 @@ $updateUserRoleQuery = "ALTER ROLE db_datareader ADD MEMBER " + $dbUserName + ";
                         "ALTER ROLE db_ddladmin ADD MEMBER " + $dbUserName + ";"
 $newSchemaQuery = "CREATE SCHEMA " + $dbUserName + " AUTHORIZATION " + $dbUserName
 
-Invoke-Sqlcmd -ServerInstance '(local)' -Database 'Model' -Query $newLoginQuery
-Invoke-Sqlcmd -ServerInstance '(local)' -Database 'Model' -Query $newUserQuery
-Invoke-Sqlcmd -ServerInstance '(local)' -Database 'Model' -Query $updateUserRoleQuery
-Invoke-Sqlcmd -ServerInstance '(local)' -Database 'Model' -Query $newSchemaQuery
+Invoke-Sqlcmd -ServerInstance '(local)' -Database 'Model' -Query $newLoginQuery | Out-File -Append C:\Informatica\Archive\scripts\createdbusers.log
+Invoke-Sqlcmd -ServerInstance '(local)' -Database 'Model' -Query $newUserQuery | Out-File -Append C:\Informatica\Archive\scripts\createdbusers.log
+Invoke-Sqlcmd -ServerInstance '(local)' -Database 'Model' -Query $updateUserRoleQuery | Out-File -Append C:\Informatica\Archive\scripts\createdbusers.log
+Invoke-Sqlcmd -ServerInstance '(local)' -Database 'Model' -Query $newSchemaQuery | Out-File -Append C:\Informatica\Archive\scripts\createdbusers.log
