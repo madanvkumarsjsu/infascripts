b4a1b312a71e7a543f8675363fdf300f1b76cdc2
vijayanand17 <vijayanand.p@live.com>
2015-11-21 12:12:38 +0530

    Checking SQL server availablity

--- createsqldbuser.ps1
+++ createsqldbuser.ps1
@@ -5,6 +5,29 @@ Param(
 
 netsh advfirewall firewall add rule name="Informatica_PC_MMSQL" dir=in action=allow profile=any localport=1433 protocol=TCP
 
+$connectionString = "Data Source=localhost;Integrated Security=true;Initial Catalog=model;Connect Timeout=3;"
+$sqlConn = new-object ("Data.SqlClient.SqlConnection") $connectionString
+$sqlConn.Open()
+
+$tryCount = 0
+while($sqlConn.State -ne "Open" -And $tryCount -lt 10)
+{
+	Start-Sleep -s 30
+	$sqlConn.Open()
+	$tryCount = $tryCount + 1
+}
+
+if ($sqlConn.State -eq 'Open')
+{
+	$sqlConn.Close();
+	"Connection to MSSQL Server succeed."
+}
+else
+{
+    "Connection to MSSQL Server failed."
+}
+
+
 $newLoginQuery = "CREATE LOGIN " + $dbUserName +  " WITH PASSWORD = '" + $dbPassword + "'"
 $newUserQuery = "CREATE USER " + $dbUserName + " FOR LOGIN " + $dbUserName + " WITH DEFAULT_SCHEMA = " + $dbUserName
 $updateUserRoleQuery = "ALTER ROLE db_datareader ADD MEMBER " + $dbUserName + ";" + 
