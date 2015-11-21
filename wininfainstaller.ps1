d2eac70b11ef2048bfa2f7d2b4309cb071895bf7
vijayanand17 <vijayanand.p@live.com>
2015-11-21 22:56:15 +0530

    Moving create DB here

--- wininfainstaller.ps1
+++ wininfainstaller.ps1
@@ -24,6 +24,44 @@ Param(
 #Adding Windows firewall inbound rule
 netsh  advfirewall firewall add rule name="Informatica_PowerCenter" dir=in action=allow profile=private localport=6005-6113 protocol=TCP
 
+if($isMaster -eq 0)
+{
+	$connectionString = "Data Source=$dbHostName;Integrated Security=true;Initial Catalog=model;Connect Timeout=3;"
+	$sqlConn = new-object ("Data.SqlClient.SqlConnection") $connectionString
+	$sqlConn.Open()
+
+	$tryCount = 0
+	while($sqlConn.State -ne "Open" -And $tryCount -lt 10)
+	{
+		Start-Sleep -s 30
+		$sqlConn.Open()
+		$tryCount = $tryCount + 1
+	}
+
+	if ($sqlConn.State -eq 'Open')
+	{
+		$sqlConn.Close();
+		"Connection to MSSQL Server succeed."
+	}
+	else
+	{
+		"Connection to MSSQL Server failed."
+	}
+
+
+	$newLoginQuery = "CREATE LOGIN " + $dbUserName +  " WITH PASSWORD = '" + $dbPassword + "'"
+	$newUserQuery = "CREATE USER " + $dbUserName + " FOR LOGIN " + $dbUserName + " WITH DEFAULT_SCHEMA = " + $dbUserName
+	$updateUserRoleQuery = "ALTER ROLE db_datareader ADD MEMBER " + $dbUserName + ";" + 
+							"ALTER ROLE db_datawriter ADD MEMBER " + $dbUserName + ";" + 
+							"ALTER ROLE db_ddladmin ADD MEMBER " + $dbUserName
+	$newSchemaQuery = "CREATE SCHEMA " + $dbUserName + " AUTHORIZATION " + $dbUserName
+
+	Invoke-Sqlcmd -ServerInstance $dbHost -Database 'Model' -Query $newLoginQuery | Out-File -Append C:\Informatica\Archive\scripts\createdbusers.log
+	Invoke-Sqlcmd -ServerInstance $dbHost -Database 'Model' -Query $newUserQuery | Out-File -Append C:\Informatica\Archive\scripts\createdbusers.log
+	Invoke-Sqlcmd -ServerInstance $dbHost -Database 'Model' -Query $updateUserRoleQuery | Out-File -Append C:\Informatica\Archive\scripts\createdbusers.log
+	Invoke-Sqlcmd -ServerInstance $dbHost -Database 'Model' -Query $newSchemaQuery | Out-File -Append C:\Informatica\Archive\scripts\createdbusers.log
+}
+
 
 $CLOUD_SUPPORT_ENABLE = "1"
 
